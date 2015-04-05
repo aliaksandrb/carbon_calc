f345dc428089e12d3db11e9ee3a0e0307d36d75c
Aliaksandr Buhayeu <aliaksandr.buhayeu@gmail.com>
2015-04-05 21:35:41 +0300

    UI: navbar links active color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def category_name(id)
     Category.find(id).name
   end
+
+  def nav_link(title, path, **options)
+    css_class = if options[:class]
+      options[:class] + (current_page?(path) ? ' active' : '')
+    else
+      current_page?(path) ? 'active' : ''
+    end
+
+    link_to title, path, options.merge(class: css_class)
+  end
+
 end

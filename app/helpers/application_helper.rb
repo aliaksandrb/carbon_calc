eef392972d8ac1cb04124bd7089015677946d843
Aliaksandr Buhayeu <aliaksandr.buhayeu@gmail.com>
2015-05-08 17:28:21 +0300

    Rework login form as pop-up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,16 @@ module ApplicationHelper
     link_to title, path, options.merge(class: css_class)
   end
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
